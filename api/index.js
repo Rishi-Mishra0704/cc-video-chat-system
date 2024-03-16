84a1042bdb901830d115eaf797fc9d11962f9800
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-03-16 23:04:53 +0530

    add peerjs

--- api/index.js
+++ api/index.js
@@ -12,7 +12,13 @@ const server = http.createServer(function (req, res) {
   res.writeHead(200, { "Content-Type": "text/plain" });
   res.end("Hello world!");
 });
+const PORT = 9000;
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  allow_discovery: true,
+});
 
+app.use("/myapp", peerServer);
 server.listen(PORT, () => {
   console.log(`PeerJS server running on port ${PORT}`);
 });
